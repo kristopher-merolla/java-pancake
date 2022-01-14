79d67e4285ab7fbee8c2feb27f6b846b28cd376a
Kristopher Merolla <27740303+kristopher-merolla@users.noreply.github.com>
2022-01-13 20:54:22 -0800

    fixing some errors

--- sandbox/basics/002/coreBasics/TestBasics.java
+++ sandbox/basics/002/coreBasics/TestBasics.java
@@ -1,9 +1,9 @@
 public class TestBasics {
   public static void main(String[] args) {
     Basics tester = new Basics();
-    int[] testArray = {1,17,5,17,19,-2,4,-6,9,1};
+    // int[] testArray = {1,17,5,17,19,-2,4,-6,9,1};
     int[] testArray2 = {};
-    int[] testArray3 = {-3,-5,-1,-11};
+    // int[] testArray3 = {-3,-5,-1,-11};
     int[] testArray4 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
     int[] testArray5 = {4,5,72,134,672,1,6,348,7,23,435,6,37,34};
 
